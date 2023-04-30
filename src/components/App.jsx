5827316dc64240156d69a1a4dcca0a164f658a46
Andrii Hashenko <hashenkoaa@gmail.com>
2023-04-30 14:14:51 +0300

    add files components and code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
+import user from '../data/user.json';
+import statistics from '../data/statistics.json';
+import Profile from './Profile/Profile';
+import StatisticList from './Statistics/StatisticsList';
+import Section from './Statistics/Section';
+import friends from '../data/friends.json';
+import FriendList from './FriendList/FriendList';
+import Container from './Container/Container';
+import transactions from '../data/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Section title="Upload stats" stats={statistics}>
+        <StatisticList stats={statistics} />
+      </Section>
+
+      <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
+    </Container>
   );
-};
+}
